391163109a868094be904af85ea597aa3e048029
Knurkboi <anton.limberg@gmail.com>
2018-02-27 16:59:38 +0100

    PrintMan början

--- src/hanga_gubbe_main/printMan.java
+++ src/hanga_gubbe_main/printMan.java
@@ -1,10 +1,119 @@
 package hanga_gubbe_main;
 
 public class printMan {
-
 	
+	public static void print(int lives) {
+		
+		if(lives == 0){
+			
+			print0();
+			
+		}
+		
+		if(lives == 1){
+			
+			print1();
+			
+		}
+		
+		if(lives == 2){
+			
+			print2();
+			
+		}
+		
+		if(lives == 3){
+			
+			print3();
+			
+		}
+		
+		if(lives == 4){
+			
+			print4();
+			
+		}
+		
+		if(lives == 5){
+			
+			print5();
+			
+		}
+		
+		if(lives == 6){
+			
+			print6();
+			
+		}
+		
+		if(lives == 7){
+			
+			print7();
+			
+		}
+		
+		if(lives == 8){
+			
+			print8();
+			
+		}
+		
+		if(lives == 9){
+			
+			print9();
+			
+		}
+		
+		if(lives == 10){
+			
+			print10();
+			
+		}
+		
+	}
 	
+	public static void print0(){
+		 
+	}
 	
+	public static void print1(){
+		 
+	}
 	
+	public static void print2(){
+		 
+	}
+	
+	public static void print3(){
+		 
+	}
+	
+	public static void print4(){
+		 
+	}
+	
+	public static void print5(){
+		 
+	}
+	
+	public static void print6(){
+		 
+	}
+	
+	public static void print7(){
+		 
+	}
+	
+	public static void print8(){
+		 
+	}
+	
+	public static void print9(){
+		 
+	}
+	
+	public static void print10(){
+		 
+	}
 	
 }
