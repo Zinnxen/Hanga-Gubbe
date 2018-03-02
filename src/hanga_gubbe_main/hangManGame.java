fb6ff39bebea0ec1b42bd8fbcbf4fd861902fa70
Zinnxen <danne276@gmail.com>
2018-03-02 10:08:48 +0100

    Gjorde så rightGuess kolllar ifall win=true

--- src/hanga_gubbe_main/hangManGame.java
+++ src/hanga_gubbe_main/hangManGame.java
@@ -112,6 +112,10 @@ public class hangManGame {
 		
 		private static void rightGuess(){			
 			printMan.print(lives);
+			
+			if(guessedArr.toString() == secretWord.toString()){
+				win = true;
+			}
 		}
 
 		private static void wrongGuess(){
