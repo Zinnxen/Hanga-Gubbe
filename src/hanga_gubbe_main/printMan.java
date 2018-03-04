f33d7c15fa6ba455306ef991818bedcb5179fd46
Knurkboi <anton.limberg@gmail.com>
2018-03-04 21:40:29 +0100

    Printman fixad, tagga runka, brb

--- src/hanga_gubbe_main/printMan.java
+++ src/hanga_gubbe_main/printMan.java
@@ -77,47 +77,114 @@ public class printMan {
 	}
 	
 	public static void print0(){
-		 
+		System.out.println("   =||=========  ");
+		System.out.println("    ||   /   |   ");
+		System.out.println("    || /     O/  ");
+		System.out.println("    ||	    /|   ");
+		System.out.println("    ||      /|)  ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
+
 	}
 	
 	public static void print1(){
-		 
+		System.out.println("   =||=========  ");
+		System.out.println("    ||   /   |   ");
+		System.out.println("    || /     O/  ");
+		System.out.println("    ||	    /|   ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print2(){
-		 
+		System.out.println("   =||=========  ");
+		System.out.println("    ||   /   |   ");
+		System.out.println("    || /     O/  ");
+		System.out.println("    ||	         ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print3(){
-		 
+		System.out.println("   =||=========  ");
+		System.out.println("    ||   /   |   ");
+		System.out.println("    || /         ");
+		System.out.println("    ||	         ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print4(){
-		 
+		System.out.println("   =||=======    ");
+		System.out.println("    ||   /       ");
+		System.out.println("    || /         ");
+		System.out.println("    ||	         ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print5(){
-		 
+		System.out.println("   =||===        ");
+		System.out.println("    ||           ");
+		System.out.println("    || /         ");
+		System.out.println("    ||	         ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print6(){
-		 
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("    ||	         ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print7(){
-		 
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("    ||	         ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print8(){
-		 
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("      	         ");
+		System.out.println("    ||           ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print9(){
-		 
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("    	         ");
+		System.out.println("                 ");
+		System.out.println("    ||           ");
+		System.out.println("  *******        ");
 	}
 	
 	public static void print10(){
-		 
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("      	         ");
+		System.out.println("                 ");
+		System.out.println("                 ");
+		System.out.println("  *******        ");
 	}
 	
 }
